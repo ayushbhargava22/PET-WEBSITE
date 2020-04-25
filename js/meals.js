2a7d3d2715efd7bcb080c9d01a903eae7aa8c4ce
ayush bhargava <57057106+ayushbhargava22@users.noreply.github.com>
2020-04-25 21:30:21 +0530

    Add files via upload

---
+++ js/meals.js
@@ -0,0 +1,42 @@
+document.getElementById("drop_down_ingri").addEventListener('click', function(){
+    var box1 = document.getElementById('drop_it')
+    if(box1.style.display=="none")
+    {
+        box1.style.display = "block"
+    }
+    else
+    {
+        box1.style.display = "none"
+    }
+})
+
+document.getElementById("drop_down_ingri2").addEventListener('click', function(){
+    var box2 = document.getElementById("drop_it2")
+    if(box2.style.display == "none")
+    {
+        box2.style.display = "block"
+    }
+    else{
+        box2.style.display = "none"
+    }
+})
+document.getElementById("drop_down_ingri3").addEventListener('click', function(){
+    var box3 = document.getElementById("drop_it3")
+    if(box3.style.display == "none")
+    {
+        box3.style.display = "block"
+    }
+    else{
+        box3.style.display = "none"
+    }
+})
+document.getElementById("drop_down_ingri4").addEventListener('click', function(){
+    var box4 = document.getElementById("drop_it4")
+    if(box4.style.display == "none")
+    {
+        box4.style.display = "block"
+    }
+    else{
+        box4.style.display = "none"
+    }
+})
